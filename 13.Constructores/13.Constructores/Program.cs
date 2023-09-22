dafba2ae91b8912ef30afaae3d89befe696863a0
= <=>
2023-09-22 17:21:43 -0500

    tho comit

--- 13.Constructores/13.Constructores/Program.cs
+++ 13.Constructores/13.Constructores/Program.cs
@@ -11,9 +11,20 @@ namespace _13.Constructores
         public static void Main(string[] args) { 
         
             Coche coche1 = new Coche();
-            Coche coche2 = new Coche();
+            coche1.setTapiceria("cuero");
+            coche1.setClimatizador(true);
 
+            Console.WriteLine(coche1.getInfo());
 
+            // otra instancia
+
+            Console.WriteLine("");
+            Coche coche2 = new Coche(5, 3);
+
+            coche2.setTapiceria("tela");
+            coche2.setClimatizador(false);
+
+            Console.WriteLine(coche2.getInfo());
         }
     }
 
@@ -26,12 +37,13 @@ namespace _13.Constructores
         //constructor = metodo utilizado para inicializar o modificar los miembros de la clase
         public Coche() {
             this.ruedas = 4;
-            this.largo = 5;
+            this.largo = 4;
             this.ancho = 2;
         }
         //sobrecarga del constructor
         public Coche(int largo, int ancho)
         {
+            this.ruedas = 4;
             this.largo=largo;
             this.ancho=ancho;
         }
@@ -45,6 +57,43 @@ namespace _13.Constructores
         public int getRuedas()
         {
             return this.ruedas;
+        }
+
+        public double getLargo()
+        {
+            return this.largo;
+        }
+
+        public double getAncho()
+        {
+            return this.ancho;
+        }
+
+        public bool getClimatizador()
+        {
+            return this.climatizador;
+        }
+        public void setClimatizador(bool climatizador)
+        {
+            this.climatizador = climatizador;
+        }
+
+        //tapiceria
+        public String getTapiceria()
+        {
+            return this.tapiceria;
+        }
+        public void setTapiceria(String tapiceria)
+        {
+            this.tapiceria = tapiceria;
+        }
+
+        public string getInfo()
+        {
+            return "Info: \n " +
+                "Ruedas: " + this.ruedas
+                + "\n Largo: " + this.largo + " \n Ancho: " + this.ancho + " \n Cliatizador: " + this.climatizador
+                + "\n Tapiceria: " + this.tapiceria; 
         }
     }
 }
