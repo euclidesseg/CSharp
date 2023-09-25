e5c87c8bbc632bbaf48fe8b37d917820dbf2b38e
= <=>
2023-09-25 17:33:19 -0500

    app avisos

---
+++ 20.Interfacesll/20.Interfacesll/AvisosTrafico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Interfacesll
+{
+    public class AvisosTrafico : IAvsiso
+    {
+
+        public AvisosTrafico() { }
+        private string remitente;
+        private string fechaAviso;
+        private string mensaje;
+        public void getFecha()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void mostrarAviso()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
